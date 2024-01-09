7a03c47d037f1e44236bb05272968f9b732d8479
felixAmark92 <felix.amark@gmail.com>
2024-01-09 19:12:52 +0100

    texture implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,12 @@
 #include <iostream>
-#include <valarray>
-#include <fstream>
 
 #include "glad/glad.h"
 #include "glfw3.h"
+#include "shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "layout (location = 1) in vec3 aColor;\n"
-                                 "\n"
-                                 "out vec3 ourColor;\n"
-                                 "\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "    gl_Position = vec4(aPos, 1.0);\n"
-                                 "    ourColor = aColor;\n"
-                                 "}";
 
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "in vec3 ourColor;\n"
-                                   "\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "    FragColor = vec4(ourColor, 1.0);\n"
-                                   "}";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -41,9 +23,6 @@ void processInput(GLFWwindow *window)
 
 
 int main() {
-
-
-
 
     GLFWwindow* window;
 
@@ -73,50 +52,62 @@ int main() {
 
     //Start of openGl code
 
-
-
-
     //shaders
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-
-    //link shaders
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
+    Shader ourShader("../src/shaders/basic-vertex.glsl", "../src/shaders/basic-fragment.glsl");
 
     //vertex data and buffers
-    float vertices[] = {                            //colors
-            0.0f, 0.5f, 0.0f,           1.0f, 0.0f, 0.0f,
-            0.5f, -0.5f, 0.0f,          0.0f, 1.0f, 0.0f,
-            -0.5f,  -0.5f, 0.0f,     0.0f, 0.0f, 1.0f
+    float vertices[] = {                      //colors                          //texture coords
+             0.5f,  0.5f, 0.0f,    1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+             0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+             -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f
     };
-//    unsigned int indices[] = {
-//            0, 1, 3,
-//            1, 2,3
-//    };
+
+    float texCoords[] = {
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            0.5f, 1.0f
+    };
+
+
+
+
+    unsigned int texture;
+
+    glGenTextures(1, &texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+    int width;
+    int height;
+    int nrChannels;
+    unsigned char *data = stbi_load("../textures/wall.jpg", &width, &height, &nrChannels, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0 , GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+
+    unsigned int indices[] = {
+            0, 1, 3,
+            1, 2,3
+    };
 
     unsigned int VAO;
     unsigned int VBO;
-    //unsigned int EBO;
-
+    unsigned int EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
-    //glGenBuffers(1, &EBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -126,7 +117,7 @@ int main() {
             3,
             GL_FLOAT,
             GL_FALSE,
-            6 * sizeof(float),
+            8 * sizeof(float),
             (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -135,12 +126,24 @@ int main() {
             3,
             GL_FLOAT,
             GL_FALSE,
-            6 * sizeof(float),
+            8 * sizeof(float),
             (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(
+            2,
+            2,
+            GL_FLOAT,
+            GL_FALSE,
+            8 * sizeof(float),
+            (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    float xOffset = 0;
+    bool boap = false;
 
     while (!glfwWindowShouldClose(window))
     {
@@ -151,9 +154,28 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        ourShader.use();
+        ourShader.setFloat("xOffset", xOffset);
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        if (!boap)
+        {
+            xOffset += 0.01;
+        }
+        else
+        {
+            xOffset -= 0.01;
+        }
+        if (xOffset >= 1.0f)
+        {
+            boap = true;
+        }
+        else if (xOffset <= -1.0f)
+        {
+            boap = false;
+        }
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
